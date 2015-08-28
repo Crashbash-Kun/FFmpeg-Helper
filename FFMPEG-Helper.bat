cab715fe50f4cba89a526fb8398d66791c60bb17
Crashbash-Kun <crashbash0@gmail.com>
2015-08-27 23:17:33 -0500

    Update FFMPEG-Helper.bat
    Add code that solves issue of empty variable for an input.

--- FFMPEG-Helper.bat
+++ FFMPEG-Helper.bat
@@ -1,5 +1,18 @@
 @echo off
+set input=%1
 
+if [%input%] NEQ [] (
+echo %input%
+ ) ELSE (
+Echo empty
+ )
+pause
+
+
+
+
+
+:AudioExtract
 Set /p codec=Codec (mp3,ogg,flac,ect)=
 set /p bitrate=bitrate in k (128,192,320,ect)=
-ffmpeg -i %1 -vn -ac 2 -ab %bitrate%k -f %codec% %1.%codec%
+ffmpeg -i %input% -vn -ac 2 -ab %bitrate%k -f %codec% %1.%codec%
